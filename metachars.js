b36c43e90445d17c6d952a41301c1ba4686fc245
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-13 11:50:08 +0200

    using the escape\

--- metachars.js
+++ metachars.js
@@ -6,7 +6,16 @@
 // Escaping meta chars
 
 let re1 = /s\./g;
+let re2 = /s\@/g; // @ is not a meta char
+let re3 = /s\\/g; 
+// "\" Simply says use the literal value that follows
 
-let txt = "Some words on the screens. s."
+let txt = "Some words on the screens. s. s@ s\\";
 
 console.log(txt.match(re1));
+console.log(txt.match(re2));
+console.log(txt.match(re3));
+
+
+
+
