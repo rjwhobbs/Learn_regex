2f04ee1b0ae204d1cc2ae61fdfcb14225d3c552f
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-13 10:30:55 +0200

    Regexs are just object in js

--- hw.js
+++ hw.js
@@ -1,6 +1,6 @@
 let txt = "This sentence has hello world in it";
 
-let regex1 = new RegExp("hello");
+let regex1 = /hello/; //more common to write like this
 let regex2 = /worlds/;
 
 console.log(regex1.test(txt));
