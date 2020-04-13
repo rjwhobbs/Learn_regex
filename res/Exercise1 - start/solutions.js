e3f1f489a0adfb955ab463e2ffe9493dcb3ecd61
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-13 12:37:17 +0200

    the solution with filter

---
+++ res/Exercise1 - start/solutions.js
@@ -0,0 +1,10 @@
+/*
+Using the provided array, create a second array that only includes the numbers with the 801 area code. (The area code is the first 3 numbers.)
+*/
+
+let phoneNums = ["801-766-9754", "801-545-5454", "435-666-1212", "801-796-8010", "435-555-9801", "801-009-0909", "435-222-8013", "801-777-6655"];
+let re_one = /801-...-..../;
+
+let newNums = phoneNums.filter(item => re_one.test(item));
+
+console.log(newNums);
