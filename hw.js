734b9792f508ea5fc60f45096cb41991cb7dd171
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-13 10:51:13 +0200

    split space

--- hw.js
+++ hw.js
@@ -2,6 +2,7 @@
 
 let regex1 = /hello/; //more common to write like this
 let regex2 = /worlds/;
+let reSpace = /\s/;
 
 // console.log(regex1.exec(txt)); // exec returns amount of matches
 // console.log(regex2.exec(txt));
@@ -12,3 +13,4 @@ console.log(txt.replace(regex1, 'hi')); // This replaces the match and returns a
 console.log(txt.split(regex1)); // Returns an array WITHOUT the match. Interestinly if there is another macth it returns an empty string. 
 // Beware of empty string returns as this will affect the length of the array,
 // Wether using a string literal or regex the result is the same.
+console.log(txt.split(reSpace));
