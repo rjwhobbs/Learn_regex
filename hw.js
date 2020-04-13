23a3e07938c5e1b259edc9ff32e78753964038b8
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-13 10:33:52 +0200

    using exec

--- hw.js
+++ hw.js
@@ -1,7 +1,7 @@
-let txt = "This sentence has hello world in it";
+let txt = "This sentence has hello world in it hello";
 
 let regex1 = /hello/; //more common to write like this
 let regex2 = /worlds/;
 
-console.log(regex1.test(txt));
-console.log(regex2.test(txt));
+console.log(regex1.exec(txt)); // exec returns amount of matches
+console.log(regex2.exec(txt));
