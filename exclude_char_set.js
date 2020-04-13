31c0f79a727ac388e18a03dbbab53ae35b8b2010
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-13 14:29:02 +0200

    Excluding meta char

--- exclude_char_set.js
+++ exclude_char_set.js
@@ -1,4 +1,7 @@
 // Exculded chars, use '^' symbol
 // only use at begining of char set
 
+let reg_one = /0x[^0-9A-F][0-9A-F]/g; // Include everything except the ones preceded buy ^
 
+let txt = "0x!F89F";
+console.log(txt.match(reg_one));
