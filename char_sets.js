ce99b65e5bca11bbd4e7e7f37644253246b91d52
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-13 12:59:17 +0200

    Meta chars inside char sets

--- char_sets.js
+++ char_sets.js
@@ -1,1 +1,14 @@
 // Charecter sets
+
+let txt = "greyc or gray won't find greay c c grey.";
+let reg_one = /gr[ae]y/g; // Match only ONE of these chars inside the char set
+// Be aware of the g flag
+let reg_two = /[abcd]/g;
+let reg_three = /[abcd][ dy]/g; // Matches only ONE char from the two combinations
+console.log(txt.match(reg_one));
+console.log(txt.match(reg_two));
+console.log(txt.match(reg_three));
+
+// NB meta char don't act as meta chars inside char sets
+let reg_four = /gr[ae]y[ .]/g; // '.' not a wildcard here
+console.log(txt.match(reg_four));
