2ef82bb585546d6af7c35677f9b5a253b2c39eab
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-13 11:40:13 +0200

    finds \t

--- wildcards.js
+++ wildcards.js
@@ -1,9 +1,10 @@
 // . Any single char
 
-let txt = "how it that so hot hiiiiiiiis h@t h t h\nt";
+let txt = "how it that so hot hiiiiiiiis h@t h t h\nt h\tt";
 
 let re1 =/h.t/g;
 
+console.log(txt);
 console.log(txt.match(re1));
 
 
