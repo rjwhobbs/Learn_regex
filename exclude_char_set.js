10956c7cc0fe11e7ab2e9e505675cca378e98f17
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-13 14:33:15 +0200

    Carrot char

--- exclude_char_set.js
+++ exclude_char_set.js
@@ -2,6 +2,8 @@
 // only use at begining of char set
 
 let reg_one = /0x[^0-9A-F][0-9A-F]/g; // Include everything except the ones preceded buy ^
+let reg_two = /[\^0-9A-F][^0-9A-F]/g; // escaped
 
 let txt = "0x!F89F";
-console.log(txt.match(reg_one));
+let txt_two = "abcdefghijklmnop^qrst";
+console.log(txt_two.match(reg_two));
