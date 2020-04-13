d4af6af4ec1f2a8a4151681ca2022ef3e27d6e09
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-13 12:41:01 +0200

    /801-/ heilds the same result as we are focusing on the first 3 chars

--- res/Exercise1 - start/solutions.js
+++ res/Exercise1 - start/solutions.js
@@ -3,7 +3,7 @@ Using the provided array, create a second array that only includes the numbers w
 */
 
 let phoneNums = ["801-766-9754", "801-545-5454", "435-666-1212", "801-796-8010", "435-555-9801", "801-009-0909", "435-222-8013", "801-777-6655"];
-let re_one = /801-...-..../;
+let re_one = /801-/;
 
 let newNums = phoneNums.filter(item => re_one.test(item));
 
