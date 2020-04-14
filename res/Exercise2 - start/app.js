270642a847e573f7fed5eace962cb580e942faa3
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-14 09:25:44 +0200

    Using strlen to double check

--- res/Exercise2 - start/app.js
+++ res/Exercise2 - start/app.js
@@ -4,12 +4,12 @@ Using the provided array, create a second array that only includes the numbers w
 
 let phoneNums = ["801-766-9754", "801-545-5454", "435-666-1212", "801-796-8010", "435-555-9801", "801-009-0909", "435-222-8013", "801-777-66553", "801-777-665-", "801-77A-6655", "801-778-665"];
 
-let regEx = /801-/,
-    newArray = [];
-//let newArray = phoneNums.filter(elem => regEx.test(elem));
+let regEx = /801-\d\d\d-\d\d\d\d/;
+let newArray = phoneNums.filter(elem => regEx.test(elem) && elem.length === 12);
 
-for (let i = 0; i < phoneNums.length; i++) {
-    if (regEx.test(phoneNums[i])) {
-        newArray.push(phoneNums[i]);
-    }
-}
+console.log(newArray);
+// for (let i = 0; i < phoneNums.length; i++) {
+//     if (regEx.test(phoneNums[i])) {
+//         newArray.push(phoneNums[i]);
+//     }
+// }
