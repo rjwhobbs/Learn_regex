cf3755d669dd17a3fb48bf18a742db57fd3b3856
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-16 18:02:42 +0200

    We can use grouping in js regex  with exec to make arrays

--- grouping.js
+++ grouping.js
@@ -1,10 +1,16 @@
 // () to group expressions
 
 let txt = "a1f3d2e3c1 aaa1d2e2f4";
-let txt2 = "Tuesday tuesdays"
+let txt2 = "Tuesday tuesdays";
+let txt3 = "2020/03/06";
 
-let reg1 = /([a-z][1-5]){5}/g; // this grouping must happen 5 time
+let reg1 = /([a-z][1-5]){5}/g; // this grouping must happen 5 times
 let reg2 = /\bmonday|tuesday|wednesday\b/gi; // == \bmonday , tuesday , wednesday\b
 let reg3 = /\b(monday|tuesday|wednesday)\b/gi; // == \bmonday\b etc
 
 console.log(txt2.match(reg3));
+
+//grouping in js
+let reg4 = /^(\d\d\d\d)[-./](\d{1,2})[-./](\d{1,2})$/
+let arr = reg4.exec(txt3);
+console.log(arr);
