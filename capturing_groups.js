12eee28565dbac6646961346dda4c48c554f2194
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-16 18:53:20 +0200

    Escaping groups

--- capturing_groups.js
+++ capturing_groups.js
@@ -1,5 +1,7 @@
 let reg1 = /(yo)\1/g; // The \1 refers to the first group, it is not refering the pattern
 let reg2 = /^(\d\d\d\d)[-./](\d{1,2})[-./]\2$/; // Now we're saying refer to the text in the 2nd group
+let reg3 = /^(\d\d\d\d)([-./])(\d{1,2})\2(\d{1,2})$/; // More useful group capturing
+let reg4 = /^(?:\d\d\d\d)([-./])(\d{1,2})\1(\d{1,2})$/; // Non capturing group add '?:' note the index change for capture number
 
 let txt = "2017-9-1";
 let txt2 = "2017-9-9";
@@ -16,3 +18,5 @@ console.log(txt2.match(reg2)); // match and you only get 2 groups in you 'split'
   groups: undefined
 ]
 */
+console.log(txt.match(reg3));
+console.log(txt.match(reg4));
