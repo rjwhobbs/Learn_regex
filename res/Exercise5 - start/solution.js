028dd1d999db5c63f333b462284674dddfd54876
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-17 11:27:32 +0200

    exe5 solution

---
+++ res/Exercise5 - start/solution.js
@@ -0,0 +1,18 @@
+/*
+Iterate through the data provided. Use a regular expression to store the names in a new array but change the order of the name so first name is listed first and last name is last. 
+*/
+
+let data = ["Jensen,", "Smith, Andrea", "Jorgensen, Michael", "Vasefi, Annika", "Lopez, Monica", "Crockett, Steven"];
+let newData = [];
+let regex = /(\w+)(\W+)(\w+)/;
+
+newData = data.map(item => {
+    let arr = regex.exec(item);
+    if (arr !== null) {
+        return arr[3] + arr[2] + arr[1];
+    } else {
+        return null;
+    }
+});
+
+console.log(newData);
