e2c99f98e5e7b6015ee96af177f380995e678b55
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-18 08:53:41 +0200

    Testing regs

--- examples.js
+++ examples.js
@@ -12,12 +12,27 @@ let newArr = arr.map(val => {
     return val.replace(/(\w+)\s+(\w+)/, "$2 *** $1");
 });
 
-let reg2 = /^(2[5][0-5]|2[0-4]\d|1\d\d|\d\d|\d)$/;
+let reg_two = /^(2[5][0-5]|2[0-4]\d|1\d\d|\d\d|\d)$/;
+let reg_three = /^(2([5][0-5]|[0-4]\d)|1\d\d|\d\d|\d)$/;
+let result = false;
 
-let i = 0;
-while (i < 256) {
-    let test = i.toString();
-    console.log(reg2.test(test), test);
+let i = -1000;
+while (i <= 1000) {
+    let num = i.toString();
+    let test = reg_three.test(num);
+    if ((i < 0 || i > 255) && test === true) {
+        console.log("Limit error (-1000 - 1000): i = ", i,"String: ", test);
+        result = false;
+    } else if ((i >= 0 && i < 256) && test === false) {
+        console.log("False result: i = ", i, "String ", num);
+        result = false;
+    } else {
+        result = true;
+    }
     i++;
+}
+
+if (result === true) {
+    console.log("Pass");
 }
 
